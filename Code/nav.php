7848634d7f3837021d1aa64084a2a4ca0237f3a3
DonTeuni <vascovangils@gmail.com>
2024-03-11 13:38:26 +0100

    nav

--- Code/nav.php
+++ Code/nav.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Navbar</title>
+    <link rel="stylesheet" href="main.css">
 </head>
+
 <body>
-    <nav>
+    <nav class="nav_bar_color">
+        <a href="index.php">Home</a>
+        <a href="nav.php">nav</a>
+        <div class="search-container">
+            <i class="fa fa-search"></i>
+            <input type="text" name="search" placeholder="Search..">
+        </div>
 
-    
+    </nav>
 
-</nav>
-    
 </body>
+
 </html>
