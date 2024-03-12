2acd748cfac9d900c702ef8fa207c2e49617b8b9
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-03-12 15:45:02 +0100

    update

--- Code/account_registeren.php
+++ Code/account_registeren.php
@@ -18,6 +18,7 @@
     <input class="create_account_input" type="text" placeholder="Achternaam" required name="achternaam">
     <input class="create_account_input" type="Email" placeholder="Email" required name="Email">
     <input class="create_account_input" type="password" placeholder="Wachtwoord" required name="wachtwoord">
+
     <input class="submit_account" type="submit" value="Account aanmaken">
     
 </form>
