f9f4e992ec3a3695935445788c9da79b5a6345fa
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-03-12 13:36:37 +0100

    update navbar

--- Code/nav.php
+++ Code/nav.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html >
 <html lang="en">
 
 <head>
@@ -7,18 +7,18 @@
     <title>Navbar</title>
     <link rel="stylesheet" href="main.css">
 </head>
-
 <body>
-    <nav class="nav_bar_color">
-        <a href="index.php">Home</a>
-        <a href="nav.php">nav</a>
-        <div class="search-container">
-            <i class="fa fa-search"></i>
-            <input type="text" name="search" placeholder="Search..">
-        </div>
 
-    </nav>
+<nav class="Navbar_color">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><input type="search" placeholder="Search.."></li>
+        
+    </ul>
+
+
+
+</nav>
 
 </body>
-
 </html>
