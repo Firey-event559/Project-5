285a6c30b408b789de56c726f1f98df59fc8d690
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-03-13 15:32:46 +0100

    update domain validation

--- Code/main.js
+++ Code/main.js
@@ -1,19 +1,18 @@
+
 function validateEmail() {
-   var emailInput = document.getElementById("email");
-   var emailError = alert(
-    "Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcr.mbo.nl."
-  );
+  var emailInput = document.getElementById("email");
+  var emailError = "Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcr.mbo.nl.";
   var allowedDomains = ["student.zadkine.nl", "tcr.mbo.nl"];
 
   var email = emailInput.value.trim().toLowerCase();
   var domain = email.split("@")[1];
 
-    if (allowedDomains.indexOf(domain) === -1) {
-        emailError = alert( "Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcr.mbo.nl.");
-        return false;
-    } else {
-        emailError.innerHTML = '';
-        return true;
+  if (allowedDomains.indexOf(domain) === -1) {
+    emailError =  alert("Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcr.mbo.nl.");
+     return false;
+  } else {
 
-    }
-}	
+     return true;
+  }
+}
+
