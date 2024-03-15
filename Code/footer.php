457546633128992f99fa3bfac895abc696c0b84c
Vasco van Gils <vascovangils@gmail.com>
2024-03-15 15:44:01 +0100

    Footer page

---
+++ Code/footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <footer class="footer">
+
+        <p>
+            <a class="imglink" href="https://www.techniekcollegerotterdam.nl/">
+                <img class="tcrimg" src="../Images/tcr-logo.png">
+            </a>
+            <a class="copyright" href="https://www.charlotteslaw.nl/auteursrecht-copyright/">Privacy en Copyright</a>
+        </p>
+        <p class="footer-names">Gemaakt door, Ramino, Aaron en Vasco.</p>
+    </footer>
+
+
+</body>
+
+</html>
