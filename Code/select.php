67c6b6d387228b632981b7f7d86cd2d70709772e
Aaron24115 <9018817@student.zadkine.nl>
2024-03-15 14:15:43 +0100

    Select begin

---
+++ Code/select.php
@@ -0,0 +1,24 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+     require "db.php";
+
+     public function select_account() {
+
+        $db = new Database_connect();
+        $conn = $db->Dbconnection();
+
+        $select 
+
+     }
+
+     ?>
+
+    </body>
+    </html>
